dd9fe360ed811b7b0a7b849ac765a2c0c0031238
Gustavoeteczl <gustavo.barros54@etec.sp.gov.br>
2022-05-27 16:43:10 -0300

    projeto Laravel

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produtos</title>
+    <style>
+        #barra{
+        background-color: #808080;
+        }
+    </style>
+</head>
+
+<body style="text-align: center;">
+    <h1>Biblioteca Laravel!</h1>
+
+    <div id="barra">
+        | &nbsp; <a href="/cadastro">Cadastrar</a> &nbsp; | &nbsp;
+        <a href="/listar-produto/1">Listar ID 1</a> &nbsp; | &nbsp;
+        <a href="/editar-produto/id1">Editar ID 1</a> &nbsp; | &nbsp;
+        <a href="/excluir-produto/1">Excluir ID 1</a> &nbsp; |
+    </div>
+    
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,77 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Models\Produto;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('inicio');
+});
+
+Route::get('/cadastro', function () {
+    return view('cadastro');
+});
+
+Route::post('/cadastrar-produto', function(Request $request){
+
+    Produto::create([
+        'nome' => $request->nome,
+        'valor' => $request->valor,
+        'estoque' => $request->estoque
+    ]);
+
+    echo "Produto criado com sucesso!";
+
+    echo '</br></br> <button><a href="/">Voltar</a></button>';
+});
+
+Route::get('/listar-produto/{id}', function($id){
+    //dd(Produto::find($id)); //debug and die
+    $produto = Produto::find($id);
+    return view('listar', ['produto' => $produto]);
+});
+
+Route::get('/editar-produto/id{id}', function(Request $request, $id){
+    //dd(Produto::find($id)); //debug and die
+    $produto = Produto::find($id);
+    return view('editar',['produto' => $produto]);
+});
+
+Route::post('/editar-produto/{id}', function(Request $request, $id){
+    //dd($request->all());
+
+    $produto = Produto::find($id);
+
+    $produto->update([
+        'nome' => $request->nome,
+        'valor' => $request->valor,
+        'estoque' => $request->estoque
+    ]);
+
+    echo "Produto editado com sucesso!";
+ 
+    echo '</br></br> <button><a href="/">Voltar</a></button>';
+});
+
+Route::get('/excluir-produto/{id}',function($id){
+    //dd($request->all());
+    $produto = Produto::find($id);
+    $produto->delete();
+
+    echo "Produto excluido com sucesso!";
+
+    echo '</br></br> <button><a href="/">Voltar</a></button>';
+});
+
